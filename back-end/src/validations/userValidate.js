9a95278b0b82f89ba6d0b6305de400ae4181d5e2
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-28 18:16:32 -0300

    feat: :sparkles: criado método para validação cos campos do user

--- back-end/src/validations/userValidate.js
+++ back-end/src/validations/userValidate.js
@@ -1,3 +1,4 @@
+const joi = require('joi');
 const { StatusCodes } = require('http-status-codes');
 const throwMyError = require('../utils/throwMyError');
 
@@ -8,6 +9,19 @@ class UserValidate {
     if (!validRoles.includes(role)) {
       throwMyError(StatusCodes.BAD_REQUEST, 'Função de usuário inválida');
     }
+  }
+
+  static validateFields(user) {
+    const newUser = { ...user, role: user.role || 'customer' };
+  
+    const { error } = joi.object({
+      name: joi.string().required(),
+      email: joi.string().email().required(),
+      password: joi.string().required(),
+      role: joi.alternatives(['customer', 'seller', 'administrator']),
+    }).validate(newUser);
+
+    if (error) throwMyError(StatusCodes.BAD_REQUEST, 'Dados inválidos');
   }
 }
 
