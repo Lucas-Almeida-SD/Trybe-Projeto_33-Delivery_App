fd73d6e13d952a322ac3a757f7cda5e335761aa2
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-29 20:47:43 -0300

    test: :test_tube: testes para a tela de Registro finalizado

--- front-end/src/tests/integration/register.test.js
+++ front-end/src/tests/integration/register.test.js
@@ -2,34 +2,37 @@ import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../../App';
 import renderWithRouter from '../helpers/renderWithRouter';
+import dataMockFetch from '../mocks/dataMockFetch';
+import dataMockRegister from '../mocks/dataMockRegister';
 
 describe('Testes da tela de Registro', () => {
+  beforeEach(() => {
+    localStorage.clear();
+    jest.clearAllMocks();
+  });
+
   const dataTestButtonRegisterLogin = 'common_login__button-register';
   const dataTestInputNameRegister = 'common_register__input-name';
   const dataTestInputEmailRegister = 'common_register__input-email';
   const dataTestInputPasswordRegister = 'common_register__input-password';
   const dataTestButtonCreateRegister = 'common_register__button-register';
 
-  it('Deve renderizar na rota "/register', async () => {
+  it('Deve renderizar na rota "/register', () => {
     const { history } = renderWithRouter(<App />);
 
     const buttonRegister = screen.getByTestId(dataTestButtonRegisterLogin);
     userEvent.click(buttonRegister);
 
-    await waitFor(() => {
-      expect(history.location.pathname).toBe('/register');
-    });
+    expect(history.location.pathname).toBe('/register');
   });
 
-  it('Deve possuir os elementos necessários para um novo registro, ', async () => {
+  it('Deve possuir os elementos necessários para um novo registro, ', () => {
     const { history } = renderWithRouter(<App />);
 
     const buttonRegisterLogin = screen.getByTestId(dataTestButtonRegisterLogin);
     userEvent.click(buttonRegisterLogin);
 
-    await waitFor(() => {
-      expect(history.location.pathname).toBe('/register');
-    });
+    expect(history.location.pathname).toBe('/register');
 
     const inputName = screen.getByTestId(dataTestInputNameRegister);
     const inputEmail = screen.getByTestId(dataTestInputEmailRegister);
@@ -42,24 +45,94 @@ describe('Testes da tela de Registro', () => {
     expect(buttonCreateRegister).toBeInTheDocument();
   });
 
-  // it('Deve possuir os elementos necessários para um novo registro, ', async () => {
-  //   const { history } = renderWithRouter(<App />);
+  it(`Ao realizar o cadastro, deve redirecionar o usuário para a tela de 
+  produtos`, async () => {
+    global.fetch = jest.fn(dataMockFetch.mockFetchSuccess);
 
-  //   const buttonRegisterLogin = screen.getByTestId(dataTestButtonRegisterLogin);
-  //   userEvent.click(buttonRegisterLogin);
+    const { history } = renderWithRouter(<App />);
 
-  //   await waitFor(() => {
-  //     expect(history.location.pathname).toBe('/register');
-  //   });
+    const buttonRegisterLogin = screen.getByTestId(dataTestButtonRegisterLogin);
+    userEvent.click(buttonRegisterLogin);
 
-  //   const inputName = screen.getByTestId(dataTestInputNameRegister);
-  //   const inputEmail = screen.getByTestId(dataTestInputNameRegister);
-  //   const inputPassword = screen.getByTestId(dataTestInputNameRegister);
-  //   const buttonCreateRegister = screen.getByTestId(dataTestInputNameRegister);
+    expect(history.location.pathname).toBe('/register');
 
-  //   expect(inputName).toBeInTheDocument();
-  //   expect(inputEmail).toBeInTheDocument();
-  //   expect(inputPassword).toBeInTheDocument();
-  //   expect(buttonCreateRegister).toBeInTheDocument();
-  // });
+    const inputName = screen.getByTestId(dataTestInputNameRegister);
+    const inputEmail = screen.getByTestId(dataTestInputEmailRegister);
+    const inputPassword = screen.getByTestId(dataTestInputPasswordRegister);
+    const buttonCreateRegister = screen.getByTestId(dataTestButtonCreateRegister);
+
+    userEvent.type(inputName, dataMockRegister.validInputRegister.name);
+    userEvent.type(inputEmail, dataMockRegister.validInputRegister.email);
+    userEvent.type(inputPassword, dataMockRegister.validInputRegister.password);
+
+    expect(buttonCreateRegister).toBeEnabled();
+    userEvent.click(buttonCreateRegister);
+
+    await waitFor(() => {
+      expect(history.location.pathname).toBe('/customer/products');
+    });
+  });
+
+  it(`Botão de Cadastro deve estar desabilitado se o nome for 
+  inválido`, () => {
+    const { history } = renderWithRouter(<App />);
+
+    const buttonRegisterLogin = screen.getByTestId(dataTestButtonRegisterLogin);
+    userEvent.click(buttonRegisterLogin);
+
+    expect(history.location.pathname).toBe('/register');
+
+    const inputName = screen.getByTestId(dataTestInputNameRegister);
+    const inputEmail = screen.getByTestId(dataTestInputEmailRegister);
+    const inputPassword = screen.getByTestId(dataTestInputPasswordRegister);
+    const buttonCreateRegister = screen.getByTestId(dataTestButtonCreateRegister);
+
+    userEvent.type(inputName, dataMockRegister.invalidInputRegister.name);
+    userEvent.type(inputEmail, dataMockRegister.validInputRegister.email);
+    userEvent.type(inputPassword, dataMockRegister.validInputRegister.password);
+
+    expect(buttonCreateRegister).toBeDisabled();
+  });
+
+  it(`Botão de Cadatro deve estar desabilitado se o formato de email for
+  inválido`, () => {
+    const { history } = renderWithRouter(<App />);
+
+    const buttonRegisterLogin = screen.getByTestId(dataTestButtonRegisterLogin);
+    userEvent.click(buttonRegisterLogin);
+
+    expect(history.location.pathname).toBe('/register');
+
+    const inputName = screen.getByTestId(dataTestInputNameRegister);
+    const inputEmail = screen.getByTestId(dataTestInputEmailRegister);
+    const inputPassword = screen.getByTestId(dataTestInputPasswordRegister);
+    const buttonCreateRegister = screen.getByTestId(dataTestButtonCreateRegister);
+
+    userEvent.type(inputName, dataMockRegister.validInputRegister.name);
+    userEvent.type(inputEmail, dataMockRegister.invalidInputRegister.email);
+    userEvent.type(inputPassword, dataMockRegister.validInputRegister.password);
+
+    expect(buttonCreateRegister).toBeDisabled();
+  });
+
+  it(`Botão de Cadatro deve estar desabilitado se o password for
+  inválido`, () => {
+    const { history } = renderWithRouter(<App />);
+
+    const buttonRegisterLogin = screen.getByTestId(dataTestButtonRegisterLogin);
+    userEvent.click(buttonRegisterLogin);
+
+    expect(history.location.pathname).toBe('/register');
+
+    const inputName = screen.getByTestId(dataTestInputNameRegister);
+    const inputEmail = screen.getByTestId(dataTestInputEmailRegister);
+    const inputPassword = screen.getByTestId(dataTestInputPasswordRegister);
+    const buttonCreateRegister = screen.getByTestId(dataTestButtonCreateRegister);
+
+    userEvent.type(inputName, dataMockRegister.validInputRegister.name);
+    userEvent.type(inputEmail, dataMockRegister.validInputRegister.email);
+    userEvent.type(inputPassword, dataMockRegister.invalidInputRegister.password);
+
+    expect(buttonCreateRegister).toBeDisabled();
+  });
 });
