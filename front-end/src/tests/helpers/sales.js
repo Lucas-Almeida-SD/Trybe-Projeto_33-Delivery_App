ede93248ddd55f11dbb7a64b1b91f4451a4d1473
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-29 16:47:48 -0300

    test: :test_tube: arquivo com a lista de Sales

---
+++ front-end/src/tests/helpers/sales.js
@@ -0,0 +1,24 @@
+const sales = [
+  {
+    deliveryAddress: 'Rua Sãp Sebatião',
+    deliveryNumber: '1015',
+    id: 1,
+    saleDate: '2022-09-29T18:32:10.000Z',
+    sellerId: 2,
+    status: 'Pendente',
+    totalPrice: 14.12,
+    userId: 3,
+  },
+  {
+    deliveryAddress: 'Rua Sãp Sebatião',
+    deliveryNumber: '1015',
+    id: 2,
+    saleDate: '2022-09-30T15:27:42.000Z',
+    sellerId: 2,
+    status: 'Pendente',
+    totalPrice: 9.7,
+    userId: 3,
+  },
+];
+
+export default sales;
