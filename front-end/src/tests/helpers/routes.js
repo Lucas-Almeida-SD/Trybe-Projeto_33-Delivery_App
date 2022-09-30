8cc3e3a804e6705f70bee1621d9401f7449d240e
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-30 20:24:26 -0300

    test: :test_tube: rotas das pages

---
+++ front-end/src/tests/helpers/routes.js
@@ -0,0 +1,6 @@
+export const login = '/login';
+export const register = '/register';
+export const customerProducts = '/customer/products';
+export const customerOrders = '/customer/orders';
+export const customerOrdersById = (id) => `/customer/products/${id}`;
+export const customerCheckout = '/customer/checkout';
