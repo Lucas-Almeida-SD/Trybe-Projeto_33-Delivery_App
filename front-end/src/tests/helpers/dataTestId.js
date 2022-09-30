1aff8a1c18f3b3f2fd75727c9586da0a6edb3569
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-30 20:27:27 -0300

    test: :test_tube: arquivo para os data-testid

---
+++ front-end/src/tests/helpers/dataTestId.js
@@ -0,0 +1,32 @@
+export const inputEmailLogin = 'common_login__input-email';
+
+export const inputPasswordLogin = 'common_login__input-password';
+
+export const buttonLogin = 'common_login__button-login';
+
+export const navBarCustomerProducts = 'customer_products__element-navbar-link-products';
+
+export const navBarCustomerOrders = 'customer_products__element-navbar-link-orders';
+
+export const navBarFullName = 'customer_products__element-navbar-user-full-name';
+
+export const navBarLogout = 'customer_products__element-navbar-link-logout';
+
+export const productPriceById = (id) => `customer_products__element-card-price-${id}`;
+
+export const productImgById = (id) => `customer_products__img-card-bg-image-${id}`;
+
+export const productNameById = (id) => `customer_products__element-card-title-${id}`;
+
+export const productRemoveBtnById = (id) => (
+  `customer_products__button-card-rm-item-${id}`);
+
+export const productAddBtnById = (id) => (
+  `customer_products__button-card-add-item-${id}`);
+
+export const productInputQuantityById = (id) => (
+  `customer_products__input-card-quantity-${id}`);
+
+export const productCheckoutBtnCart = 'customer_products__button-cart';
+
+export const productCheckoutBtnValue = 'customer_products__checkout-bottom-value';
