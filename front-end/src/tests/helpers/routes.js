bc63e10058d1c8174409f61ea343c9302932c46a
lucas_almeida <lucas.almdiniz@gmail.com>
2022-10-01 22:46:14 -0300

    test: :test_tube: rota customerOrdersById corrigida

--- front-end/src/tests/helpers/routes.js
+++ front-end/src/tests/helpers/routes.js
@@ -2,5 +2,5 @@ export const login = '/login';
 export const register = '/register';
 export const customerProducts = '/customer/products';
 export const customerOrders = '/customer/orders';
-export const customerOrdersById = (id) => `/customer/products/${id}`;
+export const customerOrdersById = (id) => `/customer/orders/${id}`;
 export const customerCheckout = '/customer/checkout';
