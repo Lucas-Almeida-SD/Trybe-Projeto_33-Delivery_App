0e8708b3e775af119a0455e391dd12ca846f2591
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-01 10:31:54 -0300

    feat: criada constantes com valor das rotas

---
+++ front-end/src/helpers/routes.js
@@ -0,0 +1,13 @@
+export const home = '/';
+
+export const login = '/login';
+
+export const register = '/register';
+
+export const customerProducts = '/customer/products';
+
+export const customerCheckout = '/customer/checkout';
+
+export const sellerOrders = '/seller/orders';
+
+export const adminManage = '/admin/manage';
